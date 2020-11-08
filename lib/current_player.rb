352286488ee101d1602e862d2b11307bd6773d3c
Thor Summerall <thor.summerall@gmail.com>
2020-11-08 03:51:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 0
   
-  board.each do |board|
+  board.each do |turns|
     if turns == "X" || turns == "O"
       counter += 1
     end
